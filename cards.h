40226d69578293c83e31791232edb2278334833b
陽悦 佐藤 <y.sato@jc-21.jp>
2022-06-03 13:07:03 +0900

    myfirst commit

---
+++ cards.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+//スペード：０　ハート：１　ダイヤ：２　クラブ：３ ジョーカー：４
+//enum 列挙子
+enum suit
+{
+	spade,
+	heart,
+	diamond,
+	club,
+	joker,
+};
